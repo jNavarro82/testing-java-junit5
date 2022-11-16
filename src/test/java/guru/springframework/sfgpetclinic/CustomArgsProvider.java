d6704805c839692be0d34ba1886954707d261506
Jorge Navarro <janavarro.arteaga@atsistemas.com>
2022-11-16 16:24:27 +0100

    Junit 5 examples

---
+++ src/test/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+public class CustomArgsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of("Paul", 9, 9),
+                Arguments.of("Mark", 8, 8),
+                Arguments.of("Frank", 7, 7)
+        );
+    }
+}
--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import guru.springframework.sfgpetclinic.CustomArgsProvider;
 import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -12,6 +15,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OwnerTest implements ModelTest {
+
+    public static Stream<Arguments> givenArgs() {
+        return Stream.of(
+                Arguments.of("John", 9, 9),
+                Arguments.of("Joe", 8, 8),
+                Arguments.of("Ed", 7, 7)
+        );
+    }
 
     @Test
     void dependentAssertions() {
@@ -34,9 +45,44 @@ class OwnerTest implements ModelTest {
     }
 
     @DisplayName("Value source ")
-    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
     @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
     void customParametrizedTest(String args) {
         System.out.println(args);
+    }
+
+    @EnumSource(OwnerType.class)
+    @DisplayName("Enum source ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @CsvSource({"Jack, 1, 1", "Peter, 2, 2", "John, 3, 3 "})
+    @DisplayName("CSV source ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    void csvTest(String name, int arg, int secondArg) {
+        System.out.println(name + " : " + arg + " : " + secondArg);
+    }
+
+    @DisplayName("CsvFileSource ")
+    @CsvFileSource(resources = "/input.csv")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    void csvInputFileTest(String name, int arg, int secondArg) {
+        System.out.println(name + " : " + arg + " : " + secondArg);
+    }
+
+    @DisplayName("MethodSource ")
+    @MethodSource("givenArgs")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    void methodSourceTest(String name, int arg, int secondArg) {
+        System.out.println(name + " : " + arg + " : " + secondArg);
+    }
+
+    @DisplayName("ArgumentsSource ")
+    @ArgumentsSource(CustomArgsProvider.class)
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    void argumentsSourceTest(String name, int arg, int secondArg) {
+        System.out.println(name + " : " + arg + " : " + secondArg);
     }
 }
